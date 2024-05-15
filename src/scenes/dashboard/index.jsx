a3fa9d6ab2fa738dd55ef4450a7b875fef545905
Reza <homayunir@gmail.com>
2024-05-15 16:50:03 -0400

    Update: - Created Header.jsx component - Header.jsx added for Dashboard page

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,8 +1,14 @@
+import { Box } from "@mui/material"
+import Header from "../../components/Header"
 
 
 const DashBoard = () => {
     return (
-        <div>DashBoard</div>
+        <Box m="20px">
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Header title={"DASHBOARD"} subtitle={"Welcome to your dashboard"}/>
+            </Box>
+        </Box>
     )
 }
 
