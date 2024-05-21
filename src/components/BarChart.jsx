75c6c7f4ca574dd341f254ba4650e8dfe8fec5cc
Reza <homayunir@gmail.com>
2024-05-21 18:51:15 -0400

    Updates: - Created GeographyChart + Geography page w/ Nivo

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,45 +1,43 @@
 import {useTheme} from "@mui/material";
 import {ResponsiveBar} from "@nivo/bar";
-import { tokens } from "../theme";
+import {tokens} from "../theme";
 import {mockBarData as data} from "../data/mockData";
+import PropTypes from "prop-types";
 
 const BarChart = ({isDashboard = false}) => {
-
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
-
-
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
   return (
     <ResponsiveBar
       data={data}
       theme={{
         axis: {
-            domain: {
-                line: {
-                    stroke: colors.grey[100],
-                }
+          domain: {
+            line: {
+              stroke: colors.grey[100],
             },
-            legend: {
-                text: {
-                    fill: colors.grey[100],
-                },
+          },
+          legend: {
+            text: {
+              fill: colors.grey[100],
             },
-            ticks: {
-                line: {
-                    stroke: colors.grey[100],
-                    strokeWidth: 1,
-                },
-                text: {
-                    fill: colors.grey[100]
-                }
-            }
+          },
+          ticks: {
+            line: {
+              stroke: colors.grey[100],
+              strokeWidth: 1,
+            },
+            text: {
+              fill: colors.grey[100],
+            },
+          },
         },
         legends: {
-            text: {
-                fill: colors.grey[100],
-            },  
-        }
+          text: {
+            fill: colors.grey[100],
+          },
+        },
       }}
       keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
       indexBy="country"
@@ -144,6 +142,10 @@ const BarChart = ({isDashboard = false}) => {
       }
     />
   );
+};
+
+BarChart.propTypes = {
+  isDashboard: PropTypes.bool,
 };
 
 export default BarChart;
