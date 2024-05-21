bffdf6929c779953a57976ab56e348d8643bb85e
Reza <homayunir@gmail.com>
2024-05-21 14:25:58 -0400

    Update: - FAQ page created

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
+import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 import Calender from "./scenes/calender";
 import { Route, Routes } from "react-router-dom";
@@ -34,7 +34,7 @@ const App = () => {
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
-              {/* <Route path="/faq" element={<FAQ />} /> */}
+              <Route path="/faq" element={<FAQ />} />
               {/* <Route path="/geography" element={<Geography />} /> */}
               <Route path="/calender" element={<Calender />} />
             </Routes>
