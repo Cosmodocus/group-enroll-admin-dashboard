2c7c8707be3c07e8f12e7e4c13acb1868a9bdf78
Reza <homayunir@gmail.com>
2024-05-21 14:13:25 -0400

    Update: - Created the Calendar page with fullcalender

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Form from "./scenes/form";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calender from "./scenes/calender";
+import Calender from "./scenes/calender";
 import { Route, Routes } from "react-router-dom";
 
 const App = () => {
@@ -36,7 +36,7 @@ const App = () => {
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/faq" element={<FAQ />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
-              {/* <Route path="/calender" element={<Calender />} /> */}
+              <Route path="/calender" element={<Calender />} />
             </Routes>
           </main>
         </div>
